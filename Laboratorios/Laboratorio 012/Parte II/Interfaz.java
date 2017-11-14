f3e7e5a9db3561a68e651b2a2a1514e3fad41a56
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2017-11-13 20:41:06 -0600

    Lab12, Parte 2 continuacion

---
+++ Laboratorios/Laboratorio 012/Parte II/Interfaz.java
@@ -0,0 +1,12 @@
+/* IMPORTS */
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+
+public class Interfaz {
+	
+	
+
+}
