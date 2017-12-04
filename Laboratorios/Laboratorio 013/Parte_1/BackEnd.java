438f20b70d3ba4a64afaa2f96f95c8b845fb262a
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2017-12-03 18:59:54 -0600

    Fin de laboratorio 13

--- Laboratorios/Laboratorio 013/Parte_1/BackEnd.java
+++ Laboratorios/Laboratorio 013/Parte_1/BackEnd.java
@@ -1,29 +1,25 @@
 public class BackEnd {
 	
 	public int [][] multiplicarMatrices(int [][] matrizUno, int [][] matrizDos) {
-
-		int largo = matrizUno[0].length;
-		int ancho = matrizDos.length;
-		int [][] resultado = new int[largo][ancho];
-		int sumatoria = 0;
-
-		for (int i = 0; i < resultado.length; i++) {
-			for (int j = 0; j < resultado[i].length; j++) {
-				for (int k = 0; k < matrizUno.length; k++) {
-					// sumatoria += (matrizUno[i][j] + matrizDos[j][i]);
-					// resultado[i][j] = sumatoria;
-					resultado[i][j] += (matrizUno[i][j] + matrizDos[j][i]);
-				}
-				sumatoria = 0;
-			}
-		}
-
-		return resultado;
-
+	    int[][] resultado = new int[matrizUno.length][matrizDos[0].length];
+	    // se comprueba si las matrices se pueden multiplicar
+	    if (matrizUno[0].length == matrizDos.length) {
+	        for (int i = 0; i < matrizUno.length; i++) {
+	            for (int j = 0; j < matrizDos[0].length; j++) {
+	                for (int k = 0; k < matrizUno[0].length; k++) {
+	                    // aquí se multiplica la matriz
+	                    resultado[i][j] += matrizUno[i][k] * matrizDos[k][j];
+	                }
+	            }
+	        }
+	    }
+	    /**
+	     * si no se cumple la condición se retorna una matriz vacía
+	     */
+	    return resultado;
 	}
 
 	public void imprimirMatriz(int [][] matriz) {
-
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				System.out.print(matriz[i][j]);
@@ -31,7 +27,6 @@ public class BackEnd {
 			}
 			System.out.println("");
 		}
-
 	}
 
 }
--- Laboratorios/Laboratorio 013/Parte_1/LaboratorioTrece.java
+++ Laboratorios/Laboratorio 013/Parte_1/LaboratorioTrece.java
@@ -7,15 +7,11 @@ public class LaboratorioTrece {
 		BackEnd bkLabTrece = new BackEnd();
 
 		/* VARIABLES */
-		int [][] array1 = {{1,0,2},{-1,3,1}};
-		int [][] array2 = {{3,1},{2,1},{1,0}};
-		int largo = array1[0].length;
-		int ancho = array2.length;
-		int [][] array3 = new int[largo][ancho];
+		int [][] matrizUno = {{1,0,2},{-1,3,1}};
+		int [][] matrizDos = {{3,1},{2,1},{1,0}};
+		int [][] matrizTres = bkLabTrece.multiplicarMatrices(matrizUno, matrizDos);
 
-		array3 = bkLabTrece.multiplicarMatrices(array1, array2);
-
-		bkLabTrece.imprimirMatriz(array3);
+		bkLabTrece.imprimirMatriz(matrizTres);
 
 	}	
 
